059016d2dba5a3e8ea695e77acce5e41dcc6d96e
Andrew Dushkov <andrew.burn1ng@gmail.com>
2019-07-24 00:16:15 +0300

    practice 3: reactive properties

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 new Vue({
+  el: "#exercise3",
+  data: {
+    value: 0
+  },
+  computed: {
+    result: function() {
+      return this.value >= 37 ? "done" : this.value;
+    }
+  },
+  watch: {
+    value: function(value) {
+      var vm = this;
+
+      setTimeout(function() {
+        vm.value = 0;
+      }, 5000);
+    }
+  }
+});
+
+new Vue({
   el: "#app_counter",
   data: {
     counter: 0,
