fc559dbee6625f8b64be41ea18b96e258997ca4e
Andrew Dushkov <andrew.burn1ng@gmail.com>
2019-07-30 00:25:53 +0300

    practice 5: conditionals and lists

--- src/conditionals.js
+++ src/conditionals.js
@@ -1,4 +1,22 @@
 new Vue({
+  el: "#exercise5",
+  data: {
+    show: false,
+    array: ["Max", "Anna", "Chris", "Manu"],
+    myObject: {
+      title: "Lord of the Rings",
+      author: "J.R.R. Tolkiens",
+      books: "3"
+    },
+    testData: {
+      name: "TESTOBJECT",
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
+
+new Vue({
   el: "#loop",
   data: {
     ingridients: ["meat", "fruit", "cookies"],
