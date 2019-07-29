b36ad103e6fafed9a36d2248fbc6d051fec035b9
Andrew Dushkov <andrew.burn1ng@gmail.com>
2019-07-29 23:01:17 +0300

    lesson 39: v-for

--- src/conditionals.js
+++ src/conditionals.js
@@ -1,4 +1,23 @@
 new Vue({
+  el: "#loop",
+  data: {
+    ingridients: ["meat", "fruit", "cookies"],
+    persons: [
+      {
+        name: "Max",
+        age: 27,
+        color: "red"
+      },
+      {
+        name: "Anna",
+        age: "N/A",
+        color: "blue"
+      }
+    ]
+  }
+});
+
+new Vue({
   el: "#app",
   data: {
     show: false
